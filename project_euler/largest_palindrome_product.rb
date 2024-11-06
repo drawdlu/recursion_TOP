5a49f66af62cbe916db19af86c00a25c97c3db88
drawdlu <152858099+drawdlu@users.noreply.github.com>
2024-11-06 20:42:27 +0800

    Check function for palindrome string

---
+++ project_euler/largest_palindrome_product.rb
@@ -0,0 +1,11 @@
+# Find largest palindrome product of 3 digit numbers
+
+def palindrome?(string)
+  return true if string.length <= 1
+
+  return false unless string[0] == string[-1]
+
+  palindrome?(string[1..-2])
+end
+
+p palindrome?('9090')
