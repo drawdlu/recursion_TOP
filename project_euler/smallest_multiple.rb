050487cf827424f9b360cc22898254f2392a07be
drawdlu <152858099+drawdlu@users.noreply.github.com>
2024-11-07 18:25:55 +0800

    Compute smallest multiple of two numbers
    Used function from previous exercise to get factors (GCD)

--- project_euler/smallest_multiple.rb
+++ project_euler/smallest_multiple.rb
@@ -4,4 +4,12 @@ def gcd(first_digit, second_digit)
   (factors(first_digit) & factors(second_digit)).max
 end
 
-# p smallest_multiple(10)
+def smallest_multiple(first_digit, second_digit = first_digit - 1)
+  return 2 if second_digit == 1
+
+  second_digit = smallest_multiple(second_digit, second_digit - 1)
+
+  (first_digit * second_digit) / gcd(first_digit, second_digit)
+end
+
+p smallest_multiple(20)
